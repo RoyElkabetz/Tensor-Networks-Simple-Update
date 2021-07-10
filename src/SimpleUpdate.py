1a1772f55e671468b9fc93dde96d4e300b2a176b
Roy Elkabetz <elkabetzroy@gmail.com>
2021-07-10 16:29:06 +0300

    fix time evolution field 0.25 factor

--- src/SimpleUpdate.py
+++ src/SimpleUpdate.py
@@ -4,7 +4,7 @@ from scipy import linalg
 from TensorNetwork import TensorNetwork
 
 
-def simple_update(tensor_network: TensorNetwork, dt: np.float, j_ij: np.float, h_k: np.float, s_i: list, s_j: list,
+def simple_update(tensor_network: TensorNetwork, dt: np.float, j_ij: list, h_k: np.float, s_i: list, s_j: list,
                   s_k: list, d_max: np.int):
     """
 
@@ -37,54 +37,45 @@ def simple_update(tensor_network: TensorNetwork, dt: np.float, j_ij: np.float, h
         ti['tensor'] = absorb_weights(ti['tensor'], i_edges_dims, weights)
         tj['tensor'] = absorb_weights(tj['tensor'], j_edges_dims, weights)
 
-        # permuting the indices associated with edge ek tensors ti, tj with their 1st index
+        # permuting the indices associated with edge ek tensors ti, tj with their 1st dimension (for convenience)
         ti = tensor_dim_permute(ti)
         tj = tensor_dim_permute(tj)
 
-        # Group all virtual indices Em!=ek to form Pl, Pr MPS tensors
-        Pl = rank_n_rank_3(ti[0])
-        Pr = rank_n_rank_3(tj[0])
+        # Group all virtual indices Em!=ek to form pi, pj MPS tensors
+        pi = rank_n_rank_3(ti['tensor'])
+        pj = rank_n_rank_3(tj['tensor'])
 
-        # SVD decomposing of Pl, Pr to obtain Q1, R and Q2, L sub-tensors, respectively
-        R, sr, Q1 = truncation_svd(Pl, [0, 1], [2], keepS='yes')
-        L, sl, Q2 = truncation_svd(Pr, [0, 1], [2], keepS='yes')
-        R = R.dot(np.diag(sr))
-        L = L.dot(np.diag(sl))
+        # # SVD decomposing of pi, pj to obtain qi, ri and qj, rj sub-tensors, respectively
+        # ri, sr, qi = truncation_svd(pi, [0, 1], [2], keepS='yes')
+        # rj, sl, qj = truncation_svd(pj, [0, 1], [2], keepS='yes')
+        # ri = ri.dot(np.diag(sr))
+        # rj = rj.dot(np.diag(sl))
 
-        # RQ decomposition of Pl, Pr to obtain R, Q1 and L, Q2 sub-tensors, respectively (needs fixing)
-        #R, Q1 = linalg.rq(np.reshape(Pl, [Pl.shape[0] * Pl.shape[1], Pl.shape[2]]))
-        #L, Q2 = linalg.rq(np.reshape(Pr, [Pr.shape[0] * Pr.shape[1], Pr.shape[2]]))
+        # RQ decomposition of pi, pj to obtain ri, qi and rj, qj sub-tensors respectively.
+        ri, qi = linalg.rq(np.reshape(pi, [pi.shape[0] * pi.shape[1], pi.shape[2]]))
+        rj, qj = linalg.rq(np.reshape(pj, [pj.shape[0] * pj.shape[1], pj.shape[2]]))
 
-        # reshaping R and L into rank 3 tensors with shape (physical_dim, Ek_dim, Q(1/2).shape[0])
-        i_physical_dim = ti[0].shape[0]
-        j_physical_dim = tj[0].shape[0]
-        R = rank_2_rank_3(R, i_physical_dim)  # (i, ek, Q1) (following the dimensions)
-        L = rank_2_rank_3(L, j_physical_dim)  # (j, ek, Q2)
+        # reshaping ri and rj into rank 3 tensors with shape (spin_dim, ek_dim, q_(right/left).shape[0])
+        i_physical_dim = ti['tensor'].shape[0]
+        j_physical_dim = tj['tensor'].shape[0]
+        ri = rank_2_rank_3(ri, i_physical_dim)  # (i, ek, qi)
+        rj = rank_2_rank_3(rj, j_physical_dim)  # (j, ek, qj)
 
-        # Contract the ITE gate with R, L, and lambda_k to form theta tensor.
-        theta = time_evolution(R,
-                               L,
-                               lambda_k,
-                               ek,
-                               dt,
-                               j_ij,
-                               h_k,
-                               s_i,
-                               s_j,
-                               s_k)  # (Q1, i', j', Q2)
+        # Contract the time-evolution gate with ri, rj, and lambda_k to form a theta tensor.
+        theta = time_evolution(ri, rj, lambda_k, ek, dt, j_ij[ek], h_k, s_i, s_j, s_k)  # (qi, i', j', qj)
 
-        # Obtain R', L', lambda'_k tensors by applying an SVD to theta
+        # Obtain ri', rj', lambda'_k tensors by applying an SVD to theta
         R_tild, lambda_k_tild, L_tild = truncation_svd(theta, [0, 1], [2, 3], keepS='yes', maxEigenvalNumber=d_max)
 
         # reshaping R_tild and L_tild back to rank 3 tensor
-        R_tild = np.reshape(R_tild, (Q1.shape[0], i_physical_dim, R_tild.shape[1]))  # (Q1, i', D')
-        R_tild = np.transpose(R_tild, [1, 2, 0])  # (i', D', Q1)
-        L_tild = np.reshape(L_tild, (L_tild.shape[0], j_physical_dim, Q2.shape[0]))  # (D', j', Q2)
-        L_tild = np.transpose(L_tild, [1, 0, 2])  # (j', D', Q2)
+        R_tild = np.reshape(R_tild, (qi.shape[0], i_physical_dim, R_tild.shape[1]))  # (qi, i', D')
+        R_tild = np.transpose(R_tild, [1, 2, 0])  # (i', D', qi)
+        L_tild = np.reshape(L_tild, (L_tild.shape[0], j_physical_dim, qj.shape[0]))  # (D', j', qj)
+        L_tild = np.transpose(L_tild, [1, 0, 2])  # (j', D', qj)
 
-        # Glue back the R', L', sub-tensors to Q1, Q2, respectively, to form updated tensors P'l, P'r.
-        Pl_prime = np.einsum('ijk,kl->ijl', R_tild, Q1)
-        Pr_prime = np.einsum('ijk,kl->ijl', L_tild, Q2)
+        # Glue back the ri', rj', sub-tensors to qi, qj, respectively, to form updated tensors P'l, P'r.
+        Pl_prime = np.einsum('ijk,kl->ijl', R_tild, qi)
+        Pr_prime = np.einsum('ijk,kl->ijl', L_tild, qj)
 
         # Reshape back the P`l, P`r to the original rank-(z + 1) tensors ti, tj
         Ti_new_shape = list(ti[0].shape)
@@ -147,26 +138,30 @@ def absorb_inverse_weights(tensor, edgesNidx, weights):
 
 
 def tensor_dim_permute(tensor):
-    permutation = np.array(list(range(len(tensor[0].shape))))
-    permutation[[1, tensor[2][0]]] = permutation[[tensor[2][0], 1]]
-    tensor[0] = np.transpose(tensor[0], permutation)
+    permutation = np.arange(len(tensor['tensor'].shape))
+    permutation[[1, tensor['dim']]] = permutation[[tensor['dim'], 1]]
+    tensor['tensor'] = np.transpose(tensor['tensor'], permutation)
     return tensor
 
 
 def rank_n_rank_3(tensor):
-    if len(tensor.shape) < 3:
-        raise IndexError('Error: 00002')
+    """
+    Turn array of shape (d1, d2, d3, ..., dn) to array of shape (d1, d2, d3 * ...* dn).
+    If array shape is (d1, d2), the new shape would be (d1, d2, 1).
+    """
     shape = np.array(tensor.shape)
-    newShape = [shape[0], shape[1], np.prod(shape[2:])]
-    newTensor = np.reshape(tensor, newShape)
-    return newTensor
+    new_shape = [shape[0], shape[1]]
+    if len(shape) > 2:
+        new_shape.append(np.prod(shape[2:]))
+    else:
+        new_shape.append(1)
+    new_tensor = np.reshape(tensor, new_shape)
+    return new_tensor
 
 
-def rank_2_rank_3(tensor, physicalDimension):
-    if len(tensor.shape) != 2:
-        raise IndexError('Error: 00003')
-    newTensor = np.reshape(tensor, [physicalDimension, int(tensor.shape[0] / physicalDimension), tensor.shape[1]])
-    return newTensor
+def rank_2_rank_3(tensor, spin_dim):
+    new_tensor = np.reshape(tensor, [spin_dim, tensor.shape[0] // spin_dim, tensor.shape[1]])
+    return new_tensor
 
 
 def rank_3_rank_n(tensor, oldShape):
@@ -196,26 +191,17 @@ def truncation_svd(tensor, leftIdx, rightIdx, keepS=None, maxEigenvalNumber=None
     return U, Vh
 
 
-def time_evolution(iTensor,
-                   jTensor,
-                   middleWeightVector,
-                   commonEdge,
-                   timeStep,
-                   interactionConst,
-                   fieldConst,
-                   iOperators,
-                   jOperators,
-                   fieldOperators):
-    d = iOperators[0].shape[0]  # physical bond dimension
-    interactionHamiltonian = np.zeros((d ** 2, d ** 2), dtype=complex)
-    for i in range(len(iOperators)):
-        interactionHamiltonian += np.kron(iOperators[i], jOperators[i])
-    Hamiltonian = -interactionConst[commonEdge] * interactionHamiltonian - 0.25 * fieldConst * (np.kron(np.eye(d), fieldOperators) + np.kron(fieldOperators, np.eye(d)))  # 0.25 is for square lattice
-    unitaryGate = np.reshape(linalg.expm(-timeStep * Hamiltonian), [d, d, d, d])
-    weightMatrix = np.diag(middleWeightVector)
-    A = np.einsum(iTensor, [0, 1, 2], weightMatrix, [1, 3], [0, 3, 2])           # A.shape = (d(i), Weight_Vector, Q1)
-    A = np.einsum(A, [0, 1, 2], jTensor, [3, 1, 4], [2, 0, 3, 4])                # A.shape = (Q1, d(i), d(j), Q2)
-    theta = np.einsum(A, [0, 1, 2, 3], unitaryGate, [1, 2, 4, 5], [0, 4, 5, 3])  # theta.shape = (Q1, d(i'), d(j'), Q2)
+def time_evolution(ri, rj, lambda_k, ek, dt, j_ij, h_k, s_i, s_j, s_k):
+    p = s_i[0].shape[0]  # physical bond dimension
+    interaction_hamiltonian = np.zeros((p ** 2, p ** 2), dtype=complex)
+    for i, _ in enumerate(s_i):
+        interaction_hamiltonian += np.kron(s_i[i], s_j[i])
+    hamiltonian = -j_ij * interaction_hamiltonian - 0.25 * h_k * (np.kron(np.eye(p), s_k) + np.kron(s_k, np.eye(p)))  # 0.25 is for square lattice
+    unitaryGate = np.reshape(linalg.expm(-dt * hamiltonian), [p, p, p, p])
+    weightMatrix = np.diag(lambda_k)
+    A = np.einsum(ri, [0, 1, 2], weightMatrix, [1, 3], [0, 3, 2])           # A.shape = (p(i), Weight_Vector, Q1)
+    A = np.einsum(A, [0, 1, 2], rj, [3, 1, 4], [2, 0, 3, 4])                # A.shape = (Q1, p(i), p(j), Q2)
+    theta = np.einsum(A, [0, 1, 2, 3], unitaryGate, [1, 2, 4, 5], [0, 4, 5, 3])  # theta.shape = (Q1, p(i'), p(j'), Q2)
     return theta
 
 
