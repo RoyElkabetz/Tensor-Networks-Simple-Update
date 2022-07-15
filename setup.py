cc2830e70e67ac33d085f0141781bb4bfdeea1dd
Roy Elkabetz <elkabetzroy@gmail.com>
2022-07-15 21:06:52 +0300

    update gitignore

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,8 +12,9 @@ setup(
     author_email="elkabetzroy@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    py_modules=['tensor_network', 'simple_update', 'structure_matrix_constructor', 'utils', 'ncon', 'examples'],
-    package_dir={'': 'src'},
+    # py_modules=['tensor_network', 'simple_update', 'structure_matrix_constructor', 'utils', 'ncon', 'examples'],
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
