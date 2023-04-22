c743431135e7daa121044d15ecd2515acd0c5993
Roy Elkabetz <elkabetzroy@gmail.com>
2023-04-22 16:58:14 +0300

    Create the math_objects.py module with the spin_operators function for generating general spin x,y,z operators.

---
+++ src/tnsu/math_objects.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def spin_operators(spin: float = 0.5):
+    """
+    (S_x)_{ab} = 1/2 (\delta_{a,b+1} + \delta_{a+1,b}) \sqrt{(s+1)(a+b-1)-ab}
+    (S_y)_{ab} = i/2 (\delta_{a,b+1} - \delta_{a+1,b}) \sqrt{(s+1)(a+b-1)-ab}
+    (S_z)_{ab} =     (s + 1 - a) \delta_{a,b}
+    :param spin: a half integer for spin type
+    :retrun: the three spin operators sx, sy, sz
+    """
+    n = int(2*spin + 1)
+    assert spin > 0, f"the spin shoud be positive half integer, instead got {spin}."
+    assert (n - (2*spin + 1)) == 0, f"spin should be a half integer (i.e., 0.5, 1, 1.5, ...), instead got {spin}."
+    sx = np.zeros((n, n), dtype=np.complex)
+    sy = np.zeros((n, n), dtype=np.complex)
+    sz = np.zeros((n, n), dtype=np.complex)
+
+    for i in range(n):
+        for j in range(n):
+            a = i + 1
+            b = j + 1
+            sx[i, j] =  0.5 * (int(a == b + 1) + int(a + 1 == b)) * np.sqrt((spin + 1) * (a + b - 1) - a * b)
+            sy[i, j] = 0.5j * (int(a == b + 1) - int(a + 1 == b)) * np.sqrt((spin + 1) * (a + b - 1) - a * b)
+            sz[i, j] = (spin + 1 - a) * int(a == b)
+
+    return sx, sy, sz
